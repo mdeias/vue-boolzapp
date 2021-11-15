c812016c3c3982c40930b69b1a4ef0518999906f
mdeias <mdeias@icloud.com>
2021-11-15 19:06:00 +0100

    milestone 1

--- milestone1/js/script.js
+++ milestone1/js/script.js
@@ -3,7 +3,163 @@ const app = new Vue({
     el: '#app',
 
     data: {
-
+        contatti: [
+            {
+                nome: 'Michele',
+                avatar: 'img/avatar_1.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Matteo',
+                avatar: 'img/avatar_2.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Stefano',
+                avatar: 'img/avatar_3.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Riccardo',
+                avatar: 'img/avatar_4.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Fabio',
+                avatar: 'img/avatar_5.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Jessica',
+                avatar: 'img/avatar_6.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            },
+            {
+                nome: 'Paolo',
+                avatar: 'img/avatar_7.jpg',
+                visibile: true,
+                messaggi: [
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    },
+                    {
+                        data: '10/05/2021 12:45:34',
+                        testo: 'Hai portato a spasso il cane?',
+                        stato: 'sent'
+                    }
+                ]
+            }
+              
+        ]
     },
 
     methods:{
